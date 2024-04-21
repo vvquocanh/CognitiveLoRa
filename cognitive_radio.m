506093f0791f852dcec5761b31b3800db503ea21
Vũ Vương Quốc Anh <quanhuk0903@gmail.com>
2024-04-22 01:00:10 +0200

    Update cognitive radio

--- cognitive_radio.m
+++ cognitive_radio.m
@@ -33,6 +33,7 @@ while true
             disp("'primary_leave': the primary user is not present.");
             disp("'primary_enter': the primary user is present.");
             disp("'create_secondary': create a secondary user.");
+            disp("'draw_power_diagram': draw the power spectral density diagram.");
             disp("'exit': exit the program.");
         case "create_primary"
             [amplitude_modulated_signal, user_information] = create_primary_user(amplitude_modulated_signal, signal_length, sampling_frequency, secondary_users, secondary_users_sensing_data, threshold);
@@ -46,15 +47,17 @@ while true
             for i = 1 : length(primary_users)
                 disp(primary_users(i));
             end
-        % case "remove_primary"
-        %     index = get_index_by_input(primary_users);
-        % 
-        %     if index == -1
-        %         disp("There is no primary user with such id.");
-        %     else
-        %         amplitude_modulated_signal = process_signal(amplitude_modulated_signal, primary_users(index).amplitude_modulated_signal, sampling_frequency, true);
-        %         primary_users(index) = [];
-        %     end
+        case "remove_primary"
+            index = get_index_by_input(primary_users);
+
+            if index == -1
+                disp("There is no primary user with such id.");
+            else
+                amplitude_modulated_signal = amplitude_modulated_signal - primary_users(index).amplitude_modulated_signal;
+                figure_title = interpolate_string("Remove primary user {user_information.id}");
+                draw_power_density_diagram(amplitude_modulated_signal, sampling_frequency, figure_title);
+                primary_users(index) = [];
+            end
         % case "primary_leave"
         %     index = get_index_by_input(primary_users);
         % 
@@ -82,12 +85,20 @@ while true
         %         secondary_users_sensing_data = [secondary_users_sensing_data; sensing_data];
         %         amplitude_modulated_signal = process_signal(amplitude_modulated_signal, user_information.amplitude_modulated_signal, sampling_frequency, false);
         %     end
+        case "draw_power_diagram"   
+            figure_title = interpolate_string("Power spectral density");
+            draw_power_density_diagram(amplitude_modulated_signal, sampling_frequency, figure_title);
         otherwise
             disp("Command not found.");
     end
 end
 
 % FUNCTION
+function range = standard_signal_length(sampling_frequency)
+    lora_signal = LoRa_Tx("", 25e3, 7, 14, sampling_frequency, 0);
+    range = length(lora_signal);
+end
+
 function index = get_index_by_input(users)
     id = input("Enter user id: ", "s");
 
